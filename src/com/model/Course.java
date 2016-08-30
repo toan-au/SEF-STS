c84cb9bd320b96866e611633b7eb476e286c70b7
SpZanG <SpZanG@users.noreply.github.com>
2016-08-30 14:51:50 +1000

    Write the Course file
    Write the Course file

--- src/com/model/Course.java
+++ src/com/model/Course.java
@@ -5,4 +5,46 @@ public class Course {
 	private int credits;
 	private int latestYear;
 	private int latestSemester;
+	
+	//The course Constructor.
+	public Course(String courseID, int credits) {
+		this.courseID = courseID;
+		this.credits = credits;
+	}
+	
+	//Getting the variables
+	public String getCourseID() {
+		return courseID;
+	}
+	
+	public int getCredits() {
+		return credits;
+	}
+	
+	public int getLatestYear() {
+		return latestYear;
+	}
+	
+	public int getLatestSemester() {
+		return latestSemester;
+	}
+	
+	//Setting the course
+	public void setCourseID(String courseID) {
+		this.courseID = courseID;
+	}
+	
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+	
+	public void setLatestYear(int latestYear) {
+		this.latestYear = latestYear;
+	}
+	
+	public void setLatestSemester(int latestSemester) {
+		this.latestSemester = latestSemester;
+	}
+	
+	//Other?
 }
