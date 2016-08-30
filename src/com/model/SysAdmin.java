e646a7e5d311ae5f90800ecfbe13a50917c2f9b4
teoapc <s3514782@student.rmit.edu.au>
2016-08-30 20:56:16 +1000

    Update SysAdmin.java

--- src/com/model/SysAdmin.java
+++ src/com/model/SysAdmin.java
@@ -1,11 +1,18 @@
 package com.model;
 
-public class SysAdmin {
+public class SysAdmin extends AdvancedUser {
+	
+	public SysAdmin(String id, String name, String password)
+	{
+		super(id, name, password);
+	}
 	public void setUpRoles(User user){
+		
+		User user = new User();
 		
 	}
 	
 	public void SetUpNewProgram(Program program){
-		
+		Program program = new Program();
 	}
 }
