3f29d15d9c7ae1065a9bd3a2934b477666561280
teoapc <s3514782@student.rmit.edu.au>
2016-09-02 13:40:11 +1000

    Update FacAdminTest.java

--- test/com/model/FacAdminTest.java
+++ test/com/model/FacAdminTest.java
@@ -8,7 +8,14 @@ public class FacAdminTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		FacAdmin facAd1 = new FacAdmin("s3514782", "Ana Teo", "password");
+                String facAd1Id = facAd1.getId();
+                String facAd1Name = facAd1.getName();
+                String facAd1Pass = facAd1.getPassword(); 
+                
+                assertEquals("s351478", facAd1Id);
+                assertEquals("Ana Teo", facAd1Name);
+                assertEquals("password", facAd1Pass);
 	}
 
 }
