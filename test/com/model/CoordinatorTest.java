2ca348c52615854d9d556a0e63b700db5efc6bc8
teoapc <s3514782@student.rmit.edu.au>
2016-09-02 14:07:06 +1000

    Update CoordinatorTest.java

--- test/com/model/CoordinatorTest.java
+++ test/com/model/CoordinatorTest.java
@@ -8,7 +8,17 @@ public class CoordinatorTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Coordinator coor = new Coordinator("s314782", "Ana Teo", "password", true);
+                String coorId = coor.getId();
+                String coorName = coor.getName();
+                String coorPass = coor.getPassword();
+                Boolean coorMeetRequirements = coor.getMeetRequirements(); 
+                
+                assertEquals("s3514782", coorId);
+                assertEquals("Ana Teo", coorName);
+                assertEquals("password", coorPass);
+                assertEquals(true, coorMeetRequirements);
+                
 	}
 
 }
