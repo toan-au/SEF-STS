d6a9559f3e9db5f1eb9a5a83469fbf0f1467ef97
SophieAu <SophieAu@users.noreply.github.com>
2016-09-04 23:34:52 +1000

    Added comment explaining lack of unit tests

--- test/com/model/ProgramTest.java
+++ test/com/model/ProgramTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class ProgramTest {
 
+	//since there are no "functional" methods in the class (yet), unit tests are not relevant (yet)
 	@Test
 	public void test() {
 		fail("Not yet implemented");
