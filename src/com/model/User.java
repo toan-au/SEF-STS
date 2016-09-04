39ff33b2c5821fd06ce3f02d75c7cc9b6eca1ace
SophieAu <SophieAu@users.noreply.github.com>
2016-09-04 23:30:47 +1000

    Deleted overly fancy formatting (Sorry cuz ;-))

--- src/com/model/User.java
+++ src/com/model/User.java
@@ -6,60 +6,43 @@ public abstract class User {
 	protected String name;
 	protected String id;
 	protected String password;
-	
-	//*********************************************************
-	//Constructor
-	//**********************************************************
-	
-	//Is the ID going to be generated or assigned??
+
+	// TODO: Is the ID going to be generated or assigned??
 	public User(String id, String name, String password) {
 		this.id = id;
 		this.name = name;
 		this.password = password;
 	}
-	
-	//*********************************************************
-	//Getters
-	//**********************************************************
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
-	//*********************************************************
-	//Setters
-	//**********************************************************
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	//*********************************************************
-	//Other
-	//**********************************************************
-	
-	public Map<Course, Double> checkStudentResults(Student student){
+
+	public Map<Course, Double> checkStudentResults(Student student) {
 		return student.getCourses();
 	}
-	
-	public boolean meetsCourseCompletion(Student student, Course course){
+
+	public boolean meetsCourseCompletion(Student student, Course course) {
 		return false;
-		
 	}
 }
