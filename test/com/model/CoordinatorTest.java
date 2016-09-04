bf6c9de9bfb91e36eefacdf99c514e7fe8330c56
SophieAu <SophieAu@users.noreply.github.com>
2016-09-05 00:45:52 +1000

    Fixed code

--- test/com/model/CoordinatorTest.java
+++ test/com/model/CoordinatorTest.java
@@ -8,17 +8,14 @@ public class CoordinatorTest {
 
 	@Test
 	public void test() {
-		Coordinator coor = new Coordinator("s314782", "Ana Teo", "password", true);
-                String coorId = coor.getId();
-                String coorName = coor.getName();
-                String coorPass = coor.getPassword();
-                Boolean coorMeetRequirements = coor.getMeetRequirements(); 
-                
-                assertEquals("s3514782", coorId);
-                assertEquals("Ana Teo", coorName);
-                assertEquals("password", coorPass);
-                assertEquals(true, coorMeetRequirements);
-                
+		Coordinator coor = new Coordinator("s314782", "Ana Teo", "password");
+		String coorId = coor.getId();
+		String coorName = coor.getName();
+		String coorPass = coor.getPassword();
+
+		assertEquals("s3514782", coorId);
+		assertEquals("Ana Teo", coorName);
+		assertEquals("password", coorPass);
 	}
 
 }
