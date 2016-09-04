1856618012b462a79f54757d762db2db8704b7b8
SophieAu <SophieAu@users.noreply.github.com>
2016-09-05 00:36:00 +1000

    Deleted stuff that wasn't supposed to be there

--- src/com/model/Coordinator.java
+++ src/com/model/Coordinator.java
@@ -4,49 +4,28 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class Coordinator extends AdvancedUser {
-	
-	private boolean meetRequirements;
-	
-	public Coordinator(String id, String name, String password, boolean meetRequirements)
-	{
+
+	public Coordinator(String id, String name, String passwords) {
 		super(id, name, password);
-		this.meetRequirements = meetRequirements;
 	}
-	
-	public boolean getMeetRequirements()
-	{
-		return meetRequirements;
-	}
-	
-	public void setMeetRequirements(boolean meetRequirements)
-	{
-		this.meetRequirements = meetRequirements;
-          
-	}
-	
-	public ArrayList<Student> meetsProgramRequirement(File file, boolean meetRequirements) 
-	{
-		//Commented textfile is filepath
-		//Read each student from file
+
+	public ArrayList<Student> meetsProgramRequirement(File file, boolean meetRequirements) {
+		// Commented textfile is filepath
+		// Read each student from file
 		Scanner student = new Scanner(new File("/*students.txt*/"));
 		ArrayList<Student> students = new ArrayList<Student>();
-		while (student.hasNext())
-		{
+		while (student.hasNext()) {
 			students.add(student.next());
 		}
 		student.close();
-		//test - Student Ana = new Student(1, 12)
-		//test - students.add(Ana);
-		//tempo code to get an idea of what's going to happen in this block
-		for (Student s:students)
-		{
-			if(s.getCredits() == 12)
-			{
+		// test - Student Ana = new Student(1, 12)
+		// test - students.add(Ana);
+		// tempo code to get an idea of what's going to happen in this block
+		for (Student s : students) {
+			if (s.getCredits() == 12)
 				System.out.println("The student meets the program requirements.");
-			}
 		}
-		
-		return students;
 
+		return students;
 	}
 }
