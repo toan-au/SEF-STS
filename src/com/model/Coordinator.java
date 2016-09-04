efafbc6dff42327a5047d5e27be8d35eb51b9a4f
SophieAu <SophieAu@users.noreply.github.com>
2016-09-05 00:44:47 +1000

    Fixed meetsProgramRequirement() code somewhat

--- src/com/model/Coordinator.java
+++ src/com/model/Coordinator.java
@@ -1,29 +1,37 @@
 package com.model;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Coordinator extends AdvancedUser {
 
-	public Coordinator(String id, String name, String passwords) {
+	public Coordinator(String id, String name, String password) {
 		super(id, name, password);
 	}
 
 	public ArrayList<Student> meetsProgramRequirement(File file, boolean meetRequirements) {
-		// Commented textfile is filepath
-		// Read each student from file
-		Scanner student = new Scanner(new File("/*students.txt*/"));
+		Scanner student;
 		ArrayList<Student> students = new ArrayList<Student>();
-		while (student.hasNext()) {
-			students.add(student.next());
-		}
-		student.close();
-		// test - Student Ana = new Student(1, 12)
-		// test - students.add(Ana);
-		// tempo code to get an idea of what's going to happen in this block
-		for (Student s : students) {
-			if (s.getCredits() == 12)
+
+		try {
+			student = new Scanner(new File("/*students.txt*/"));
+
+			while (student.hasNext()) {
+				// students.add(student.next());
+			}
+			student.close();
+			// test - Student Ana = new Student(1, 12)
+			// test - students.add(Ana);
+			// tempo code to get an idea of what's going to happen in this block
+			for (Student s : students) {
+				// if (s.getCredits() == 12)
 				System.out.println("The student meets the program requirements.");
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 
 		return students;
