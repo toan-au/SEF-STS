7fbaff080ba473e4f9256517ef2d8a1b931af0e9
SophieAu <SophieAu@users.noreply.github.com>
2016-09-05 00:39:40 +1000

    Deleted stuff that wasn't supposed to be there

--- test/com/model/AdvancedUserTest.java
+++ test/com/model/AdvancedUserTest.java
@@ -8,21 +8,15 @@ public class AdvancedUserTest {
 
 	@Test
 	public void test() {
-		AdvancedUser advUser = new AdvancedUser("s3514782","Ana Teo","password", 123, "Super special");
-                String advUserId = advUser.getId();
-                String advUserName = advUser.getName();
-                String advUserPass = advUser.getPassword();
-                int advUserCreds = advUser.getCredits();
-                String advUserSpecialization = advUser.getSpecialization();
-                
-                assertEquals("s3514782", advUserId);
-                assertEquals ("Ana Teo", advUserName);
-                assertEquals("password", advUserPass);
-                assertEquals(123, advUserCreds);
-                assertEquals("Super special", advUserSpecialization);
-                
-                
-                
+		AdvancedUser advUser = new AdvancedUser("s3514782", "Ana Teo", "password");
+		String advUserId = advUser.getId();
+		String advUserName = advUser.getName();
+		String advUserPass = advUser.getPassword();
+
+		assertEquals("s3514782", advUserId);
+		assertEquals("Ana Teo", advUserName);
+		assertEquals("password", advUserPass);
+
 	}
 
 }
