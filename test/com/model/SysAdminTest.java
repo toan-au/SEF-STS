7c5966142abb8b456621e942712abaa00f01e31c
SophieAu <SophieAu@users.noreply.github.com>
2016-09-05 00:13:02 +1000

    Tests cannot be finished because needed methods doesn't exist yet
    I just realized while writing the test that the method is missing

--- test/com/model/SysAdminTest.java
+++ test/com/model/SysAdminTest.java
@@ -12,40 +12,47 @@ public class SysAdminTest {
 	SysAdmin testAdmin;
 
 	@BeforeClass
-	public void setUp(){
+	public void setUp() {
 		testAdmin = new SysAdmin("123", "test", "test");
 	}
 
 	@Test
 	public void newFacAdminTest() {
 		FacAdmin testFac = new FacAdmin("124", "test", "test");
-		
+		testAdmin.setUpRoles(testFac);
+
+		// find new user in Database. Code to do this is not yet planned out
 	}
 
 	@Test
 	public void newSysAdminTest() {
-		SysAdmin testFac = new SysAdmin("125", "test", "test");
-		
+		SysAdmin testSys = new SysAdmin("125", "test", "test");
+		testAdmin.setUpRoles(testSys);
+
+		// find new user in Database. Code to do this is not yet planned out
 	}
 
 	@Test
 	public void newCoordinatorTest() {
 		Coordinator testCoordinator = new Coordinator("126", "test", "test");
-		
+		testAdmin.setUpRoles(testCoordinator);
+
+		// find new user in Database. Code to do this is not yet planned out
 	}
 
-	//this test will fail as the Date data type in Student is deprecated (to be replaced by Calendar)
 	@Test
 	public void newStudentTest() {
 		Student testStudent = new Student("125", "test", "test", new Calendar().set(01, 01, 1996), "a@b.com");
-		
+		testAdmin.setUpRoles(testStudent);
+
+		// find new user in Database. Code to do this is not yet planned out
 	}
-	
+
 	@Test
 	public void newProgramTest() {
 		Program testProgram = new Program();
 		testAdmin.setUpNewProgram(testProgram);
-		
+
 		assertTrue(testProgram.getIsActive());
 	}
 
