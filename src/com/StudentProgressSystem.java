69f012c91faab08d0d92e463adfac444b10cbd90
DeftCorgi <toanau123@gmail.com>
2016-09-09 09:51:00 +1000

    Wrote some pseudocode for user validation

--- src/com/StudentProgressSystem.java
+++ src/com/StudentProgressSystem.java
@@ -14,4 +14,16 @@ public class StudentProgressSystem {
 		
 	}
 	
+	private void validate() {
+		// prompt User for login
+		
+		// save user input as variables
+		
+		// see if the id given exists with Storage.getUser()
+		
+		// id it does then see if proided password matches the returned User's password
+		
+		// set currentUser to the returned User otherwise keep looping
+	}
+	
 }
