8c798d760256c11e2251527528ea000326093ab8
SophieAu <SophieAu@users.noreply.github.com>
2016-09-09 15:20:34 +1000

    Added a 'fancyAdminMode' for us to go around the validation process
    Will be deleted later, once we have added all the users into persistent
    storage

--- src/com/StudentProgressSystem.java
+++ src/com/StudentProgressSystem.java
@@ -3,38 +3,50 @@ package com;
 import java.util.Scanner;
 
 import com.model.Storage;
+import com.model.SysAdmin;
 import com.model.User;
 
 public class StudentProgressSystem {
 
 	User currentUser;
+	Scanner logInScanner;
 
 	public StudentProgressSystem() {
-		validate();
+		logInScanner = new Scanner(System.in);
+		System.out.println("Welcome to this thing. Press Enter to begin.");
+		if (logInScanner.nextLine().equals("UpUpDownDownLeftRightLeftRightBA"))
+			fancyAdminMode();
+		SysAdmin fancyAdmin = new SysAdmin("temp", "temp", "temp");
+	}
+
+	private void fancyAdminMode() {
+		System.out.println("fancy admin mode activated");
+		// TODO Auto-generated method stub
 
 	}
 
 	public void run() {
+		validate();
 
+		logInScanner.close();
 	}
 
 	private void validate() {
-		Scanner scanner = new Scanner(System.in);
 		boolean isLoggedIn = false;
 		User tempUser;
-		
+
 		System.out.println("Please enter your account details");
 		while (!isLoggedIn) {
 			System.out.println("User Name: ");
-			tempUser = Storage.getUser(scanner.next());
+			tempUser = Storage.getUser(logInScanner.nextLine());
 			if (tempUser == null) {
 				System.out.println("The User Name you entered does not exist. Please try again");
 				continue;
 			}
 
 			System.out.println("Pasword: ");
-			String passwordEntry = scanner.next();
-			if (tempUser.getPassword() != passwordEntry) {
+			String passwordEntry = logInScanner.nextLine();
+			if (!tempUser.getPassword().equals(passwordEntry)) {
 				System.out.println("The Password you entered does not match the user name. Please try again");
 				continue;
 			}
@@ -43,7 +55,6 @@ public class StudentProgressSystem {
 			System.out.println("Logging in...");
 			isLoggedIn = true;
 		}
-		scanner.close();
 	}
 
 }
