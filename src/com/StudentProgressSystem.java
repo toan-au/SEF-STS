8d6a94a4f83f1052760bbf4908d7da5f7fb63233
SophieAu <SophieAu@users.noreply.github.com>
2016-09-09 14:07:38 +1000

    Implemented Console Log-in

--- src/com/StudentProgressSystem.java
+++ src/com/StudentProgressSystem.java
@@ -1,29 +1,49 @@
 package com;
 
+import java.util.Scanner;
+
+import com.model.Storage;
 import com.model.User;
 
 public class StudentProgressSystem {
-	
+
 	User currentUser;
-	
+
 	public StudentProgressSystem() {
-		
+		validate();
+
 	}
-	
+
 	public void run() {
-		
+
 	}
-	
+
 	private void validate() {
-		// prompt User for login
+		Scanner scanner = new Scanner(System.in);
+		boolean isLoggedIn = false;
+		User tempUser;
 		
-		// save user input as variables
-		
-		// see if the id given exists with Storage.getUser()
-		
-		// id it does then see if proided password matches the returned User's password
-		
-		// set currentUser to the returned User otherwise keep looping
+		System.out.println("Please enter your account details");
+		while (!isLoggedIn) {
+			System.out.println("User Name: ");
+			tempUser = Storage.getUser(scanner.next());
+			if (tempUser == null) {
+				System.out.println("The User Name you entered does not exist. Please try again");
+				continue;
+			}
+
+			System.out.println("Pasword: ");
+			String passwordEntry = scanner.next();
+			if (tempUser.getPassword() != passwordEntry) {
+				System.out.println("The Password you entered does not match the user name. Please try again");
+				continue;
+			}
+
+			currentUser = tempUser;
+			System.out.println("Logging in...");
+			isLoggedIn = true;
+		}
+		scanner.close();
 	}
-	
+
 }
