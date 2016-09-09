36b2c6b67a6b5cb8031918c197a110f78921bd39
SophieAu <SophieAu@users.noreply.github.com>
2016-09-09 14:16:17 +1000

    Tidied the Storage class

--- src/com/model/Storage.java
+++ src/com/model/Storage.java
@@ -3,46 +3,32 @@ package com.model;
 import java.util.ArrayList;
 
 public class Storage {
-	// An arraylist of users
-	public static ArrayList<User> users = new ArrayList<User>(256);
-	public static ArrayList<Course> courses = new ArrayList<Course>(256);
-	public static ArrayList<Program> programs = new ArrayList<Program>(256);
-	
-	
-	// returns a User object if there is a match in IDs, return null otherwise
+	public static ArrayList<User> users = new ArrayList<User>();
+	public static ArrayList<Course> courses = new ArrayList<Course>();
+	public static ArrayList<Program> programs = new ArrayList<Program>();
+
 	public static User getUser(String id) {
-		if (users.isEmpty())
-			return null;
-		for (int i = 0; i < users.size(); i++) {
-			if (users.get(i) != null && users.get(i).getId() == id) {
-				return users.get(i);			// return the user if tehre is a match
-			}
+		for (User user : users) {
+			if (user.getId().equals(id))
+				return user;
 		}
 		return null;
 	}
-	
-	// Return a course object with the same id or null
+
 	public static Course getCourse(String id) {
-		if (courses.isEmpty())
-			return null;
-		for (int i = 0; i < courses.size(); i++) {
-			if (courses.get(i) != null && courses.get(i).getCourseID() == id) {
-				return courses.get(i);			// return the user if tehre is a match
-			}
+		for (Course course : courses) {
+			if (course.getCourseId().equals(id))
+				return course;
 		}
 		return null;
 	}
-	
-	// Return a course object with the same id or null
-		public static Program getProgram(String programCode) {
-			if (programs.isEmpty())
-				return null;
-			for (int i = 0; i < programs.size(); i++) {
-				if (programs.get(i) != null && programs.get(i).getProgramCode() == programCode) {
-					return programs.get(i);			// return the user if tehre is a match
-				}
-			}
-			return null;
+
+	public static Program getProgram(String programCode) {
+		for (Program program : programs) {
+			if (program.getProgramCode().equals(programCode))
+				return program;
 		}
-	
+		return null;
+	}
+
 }
