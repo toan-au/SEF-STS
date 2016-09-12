6086535faf0e5c4fa723e79b2f86ddc5bf8cd9cc
SophieAu <SophieAu@users.noreply.github.com>
2016-09-12 19:24:55 +1000

    Storage is now initalized from the Starter class

--- src/com/Starter.java
+++ src/com/Starter.java
@@ -1,9 +1,13 @@
 package com;
 
+import com.model.Storage;
+import com.view.StudentProgressSystem;
+
 public class Starter {
 
 	public static void main(String[] args) {
 		StudentProgressSystem sps = new StudentProgressSystem();
+		Storage.init();
 		sps.run();
 	}
 
