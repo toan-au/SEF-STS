845b1cafae040c619988851b076f0bf37288188b
SophieAu <SophieAu@users.noreply.github.com>
2016-09-12 17:28:39 +1000

    Some minor code and format fixes

--- src/com/StudentProgressSystem.java
+++ src/com/StudentProgressSystem.java
@@ -1,5 +1,6 @@
 package com;
 
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 
 import com.model.Coordinator;
@@ -13,67 +14,66 @@ public class StudentProgressSystem {
 
 	User currentUser;
 	Scanner scanner;
+	boolean isLoggedIn = false;
 
 	public StudentProgressSystem() {
-		scanner = new Scanner(System.in);
+		Storage.init();
 	}
 
 	private void fancyAdminMode() {
-		@SuppressWarnings("unused")
-		SysAdmin fancyAdmin = new SysAdmin("temp", "temp", "temp");
 		System.out.println("fancy admin mode activated");
 
 		System.out.println("Create a new User Account:\n" + "Please add an Id:");
 		String userId = scanner.next();
 		System.out.println("Please add a password:");
 		String password = scanner.next();
-		
-		if (userId.startsWith("S")) {
-			Student newStudent = new Student(userId, password);
-			Storage.users.add(newStudent);
-			System.out.println("You have just created a student account with the user name "+userId+"and password "+password".");
-			System.out.println("This account is now ready to be used.")
-			System.out.println("You will now be taken back to the main menu...");
-			run();
-		} else if (userId.startsWith("F")) {
-			FacAdmin newFacAdmin = new FacAdmin(userId, password);
-			Storage.users.add(newFacAdmin);
-			System.out.println("You have just created a faculty admin account with the user name "+userId+"and password "+password".");
-			System.out.println("This account is now ready to be used.")
-			System.out.println("You will now be taken back to the main menu...");
-			run();
-		} else if (userId.startsWith("C")) {
-			Coordinator newCoordinator = new Coordinator(userId, password);
-			Storage.users.add(newCoordinator);
-			System.out.println("You have just created a coordinator account with the user name "+userId+"and password "+password".");
-			System.out.println("This account is now ready to be used.")
-			System.out.println("You will now be taken back to the main menu...");
-			run();
-		} else if (userId.startsWith("A")) {
-			SysAdmin newSysAdmin = new SysAdmin(userId, password);
-			Storage.users.add(newSysAdmin);
-			System.out.println("You have just created a system admin account with the user name "+userId+"and password "+password".");
-			System.out.println("This account is now ready to be used.")
-			System.out.println("You will now be taken back to the main menu...");
-			run();
-		}
-		
+		System.out.println("Please add the user's name:");
+		String fullName = scanner.nextLine();
 
+		if (userId.startsWith("a")) {
+			Storage.users.add(new SysAdmin(userId, fullName, password));
+
+		} else if (userId.startsWith("c")) {
+			Storage.users.add(new Coordinator(userId, fullName, password));
+
+		} else if (userId.startsWith("f")) {
+			Storage.users.add(new FacAdmin(userId, fullName, password));
+
+		} else if (userId.startsWith("s")) {
+
+			System.out.println("Please enter the student's birthday like so: DD/MM/YYYY");
+			scanner.useDelimiter("/");
+			int day = scanner.nextInt();
+			int month = scanner.nextInt();
+			int year = scanner.nextInt();
+			scanner.reset();
+
+			System.out.println("Please enter the student's email.");
+			String email = scanner.next();
+
+			Storage.users.add(new Student(userId, fullName, password, new GregorianCalendar(year, month, day), email));
+		} else {
+			System.out.println("The account name you have entered is not valid");
+			return;
+		}
+
+		System.out.println("You have just created an account with the user name " + userId + "and password " + password + ".\n"
+				+ "This account is now ready to be used.\n" + "You will now be taken back to the main menu...");
 	}
 
 	public void run() {
-		Storage.init();
+		scanner = new Scanner(System.in);
+
 		System.out.println("Welcome to this thing. Press Enter to begin.");
 		if (scanner.nextLine().equals("UpUpDownDownLeftRightLeftRightBA"))
 			fancyAdminMode();
 
-		validate();
-		menu();
+		logIn();
+		displayMenu();
 		scanner.close();
 	}
 
-	private void validate() {
-		boolean isLoggedIn = false;
+	private void logIn() {
 		User tempUser;
 
 		System.out.println("Please enter your account details");
@@ -87,7 +87,7 @@ public class StudentProgressSystem {
 				continue;
 			}
 
-			System.out.println("Pasword: ");
+			System.out.println("Password: ");
 			String passwordEntry = scanner.nextLine();
 			if (!tempUser.getPassword().equals(passwordEntry)) {
 				System.out.println("The Password you entered does not match the ID. Please try again");
@@ -100,8 +100,10 @@ public class StudentProgressSystem {
 		}
 	}
 
-	private void menu() {
+	private void displayMenu() {
 		String userId = currentUser.getId();
+
+		System.out.println("Hello, " + userId + ". What would you like to do?");
 
 		if (userId.startsWith("s"))
 			displayStudentMenu();
@@ -114,15 +116,13 @@ public class StudentProgressSystem {
 	}
 
 	private void displayStudentMenu() {
-		System.out.println("Hello, ." + currentUser.getId() + "What would you like to do?");
 		System.out.println("1 - Check your results\n");
 		System.out.println("2 - log out\n");
-		Scanner scanner = new Scanner(System.in);
 		int choice = scanner.nextInt();
-		
-		//set a flag for continue run the loop until log out.
+
+		// set a flag for continue run the loop until log out.
 		boolean studentFlag = true;
-		while (studentFlag){
+		while (studentFlag) {
 			switch (choice) {
 			case 1:
 				System.out.println("Here are your results...");
@@ -135,8 +135,7 @@ public class StudentProgressSystem {
 				System.out.println("Please input a valid choice");
 			}
 		}
-		
-		
+
 	}
 
 	private void displayFacAdminMenu() {
@@ -146,24 +145,23 @@ public class StudentProgressSystem {
 	}
 
 	private void displayCoordinatorMenu() {
-		System.out.println("Hello, ." + currentUser.getId() + "What would you like to do?");
-		//System.out.println("1.) Check student results\n" + "2.) Create a student account\n" + "3.) Upload enrolment");
+		// System.out.println("1.) Check student results\n" + "2.) Create a student account\n" +
+		// "3.) Upload enrolment");
 		System.out.println("1 - Check student results\n");
 		System.out.println("2 - Create a student account\n");
 		System.out.println("3 - Upload enrolment\n");
 		System.out.println("4 - log out\n");
-		Scanner scanner = new Scanner(System.in);
 		int choice = scanner.nextInt();
 
-		//set a flag for continue run the loop until log out.
+		// set a flag for continue run the loop until log out.
 		boolean coordinatorFlag = true;
-		while (coordinatorFlag){
+		while (coordinatorFlag) {
 			switch (choice) {
 			case 1:
 				// currentUser.checkStudentResults();
 				break;
 			case 2:
-				((Coordinator)currentUser).createStudentAccount();
+				((Coordinator) currentUser).createStudentAccount();
 				break;
 			case 3:
 				// currentUser.uploadEnrolment();
@@ -175,34 +173,33 @@ public class StudentProgressSystem {
 				System.out.println("Please input a valid choice");
 			}
 		}
-		
+
 	}
 
 	private void displaySysAdminMenu() {
-		//boolean to determine if we should loop
+		// boolean to determine if we should loop
 		boolean loop = true;
-		
-		while(loop) {
-			System.out.println("Hello, ." + currentUser.getId() + "\nWhat would you like to do?\n");
+
+		while (loop) {
 			System.out.println("1 - Set up roles");
 			System.out.println("2 - Set up a new program");
 			System.out.println("3 - Create a student account");
 			System.out.println("4 - Upload enrolment");
 			System.out.println("5 - log out");
-			
+
 			int choice = scanner.nextInt();
 			switch (choice) {
 			case 1:
-				//currentUser.setUpRoles();
+				// currentUser.setUpRoles();
 				break;
 			case 2:
-				((SysAdmin)currentUser).setUpNewProgram();
+				((SysAdmin) currentUser).setUpNewProgram();
 				break;
 			case 3:
-				((SysAdmin)currentUser).createStudentAccount();
+				((SysAdmin) currentUser).createStudentAccount();
 				break;
 			case 4:
-				//currentUser.uploadEnrolment();
+				// currentUser.uploadEnrolment();
 				break;
 			case 5:
 				System.out.println("Logging out...\n");
