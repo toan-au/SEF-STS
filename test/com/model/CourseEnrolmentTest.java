f0b652aa5370c1f6e7276eca424d607629b8af7c
DeftCorgi <toanau123@gmail.com>
2016-09-26 14:04:59 +1000

    Wrote some basic test cases

--- test/com/model/CourseEnrolmentTest.java
+++ test/com/model/CourseEnrolmentTest.java
@@ -8,7 +8,13 @@ public class CourseEnrolmentTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		CourseEnrolment e1 = new CourseEnrolment("s1111111", "COSC2102B", 1, 2016);
+		CourseEnrolment e2 = new CourseEnrolment("s1111111", "AERO2394", 2, 2015);
+		CourseEnrolment e3 = new CourseEnrolment("s1111111", "EEET2032", 1, 2016);
+		CourseEnrolment e4 = new CourseEnrolment("s1111111", "ISYS1013", 1, 2016);
+		
+		assertEquals("Software Engineering Project (PtB)", e1.getCourse().getCourseName());
+		assertEquals("Toan Au", e1.getStudent().getName());
 	}
 
 }
