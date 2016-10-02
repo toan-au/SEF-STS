60d08e469b0fed33e4edd9565c2a5efdc9ab9b34
spzang <=>
2016-10-02 16:23:50 +1100

    Change code to harold branch
    Change code to harold branch

--- src/com/model/AdvancedUser.java
+++ src/com/model/AdvancedUser.java
@@ -1,6 +1,7 @@
 package com.model;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Map;
 import java.util.Scanner;
@@ -79,7 +80,7 @@ public abstract class AdvancedUser extends User {
 	}
 
 	public void setCoreCourses(Program program, ArrayList<Course> course) {
-		
+
 	}
 
 	public void setSpecializationCourses(Program program, ArrayList<Course> courses, String specialization, SpecializationMode mode) {
@@ -124,7 +125,7 @@ public abstract class AdvancedUser extends User {
 	}
 
 	public void createStudentAccount(ArrayList<Student> student) {
-	
+
 	}
 
 	public void uploadEnrolment(Student student, Map<Course, Double> enrolments) {
@@ -134,4 +135,89 @@ public abstract class AdvancedUser extends User {
 	public void uploadEnrolment(Map<Student, Map<Course, Double>> enrolments) {
 
 	}
+	
+	protected void updateStudentAccount(String userId, String password, String fullName) {
+		String idInput, passwordInput, nameInput;
+		User userCheck;
+		int dayInput, monthInput, yearInput, updateInput;
+		boolean checkInputFlag, updateFlat;
+		//two flag for loop
+		updateFlat = true;
+		checkInputFlag = true;
+		//Check user is exist or not.
+		while(checkInputFlag){
+			System.out.println("Please enter the student's id that you want to change, or input exit to quit.");
+			idInput = StudentProgressSystem.scanner.next();
+			userCheck = Storage.getUser(idInput);
+			if (userCheck != null){
+				checkInputFlag = false;
+			}else if (idInput == "exit"){
+				checkInputFlag = false;
+				updateFlat = false;
+				System.out.println("Exit Update function");
+			}else{
+				System.out.println("This user is not exist, please input an exist user.");
+			}
+		}
+		
+		
+		//Using while loop to change the user's detail until exit.
+		while(updateFlat){
+			System.out.println("Which part you want to change?");
+			System.out.println("1.Email");
+			System.out.println("2.userId");
+			System.out.println("3.Password");
+			System.out.println("4.fullName");
+			System.out.println("5.exit");
+			updateInput = StudentProgressSystem.scanner.nextInt();
+			
+			if (updateInput == 1){
+				//get user input
+				System.out.println("Please enter the student's birthdate (day only)");
+				StudentProgressSystem.scanner.useDelimiter("/");
+				dayInput = StudentProgressSystem.scanner.nextInt();
+				System.out.println("Please enter the student's birth month");
+				monthInput = StudentProgressSystem.scanner.nextInt();
+				System.out.println("Please enter the student's birth year");
+				yearInput = StudentProgressSystem.scanner.nextInt();
+				StudentProgressSystem.scanner.reset();
+				//update the birthday
+				setDateOfBirth(new GregorianCalendar(yearInput, monthInput, dayInput));
+				
+			}else if (updateInput == 2){
+				//update the user id
+				System.out.println("Please enter the student's id");
+				idInput = StudentProgressSystem.scanner.next();
+				setId(idInput);
+				
+			}else if (updateInput == 3){
+				//update the password
+				System.out.println("Enter the student's password");
+				passwordInput = StudentProgressSystem.scanner.next();
+				setPassword(passwordInput);
+				
+			}else if (updateInput == 4){
+				//update user's name
+				System.out.println("Enter the student's Name");
+				nameInput = StudentProgressSystem.scanner.next();
+				setName(nameInput);
+				
+			}else if (updateInput == 5){
+				//exit the function
+				updateFlat = false;
+				System.out.println("Exit Update function");
+				
+			}else{
+				
+				System.out.println("Invalid input, please input number from 1 to 5.");
+			}
+		}
+		
+		
+		
+
+		System.out.println("Please enter the student's email.");
+		String email = StudentProgressSystem.scanner.next();
+	}
+	
 }
