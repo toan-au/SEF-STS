739dc666e9960ecb948a682a80d934fc350e00f7
DeftCorgi <toanau123@gmail.com>
2016-10-02 16:55:27 +1100

    Fixed checkStudentResults() for the User abstract class

--- src/com/model/User.java
+++ src/com/model/User.java
@@ -3,6 +3,8 @@ package com.model;
 import java.io.Serializable;
 import java.util.Map;
 
+import com.view.StudentProgressSystem;
+
 public abstract class User implements Serializable {
 	protected String name;
 	protected String id;
@@ -40,8 +42,31 @@ public abstract class User implements Serializable {
 		this.password = password;
 	}
 
-	public Map<Course, Double> checkStudentResults(Student student) {
-		return student.getCourses();
+	public void checkStudentResults() {
+		String status;
+		String studentId;
+		Student student;
+		
+		
+		System.out.println("Which student would you like to check?");
+		studentId = StudentProgressSystem.scanner.next();
+		
+		student = (Student)Storage.getUser(studentId);
+		
+		
+		System.out.println("results are:");
+		System.out.println("course ID \t semester \t year \t status");
+		
+		//iterate over the courses Arraylist and print the ID and status of the student
+		for(int i = 0; i < student.getCourses().size(); i++) {
+			// if the student has failed the course then print "failed"  otherwise "pass"
+			if(student.getCourses().get(i).isFailed()) {
+				status = "failed";
+			} else {
+				status = "pass";
+			}
+			System.out.println(student.getCourses().get(i).getCourse().getCourseId() + "\t\t" +  student.getCourses().get(i).getSemester() + "\t" + student.getCourses().get(i).getYear() +"\t" + status); 
+		}
 	}
 
 	public boolean meetsCourseCompletion(Student student, Course course) {
